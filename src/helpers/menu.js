bcf7570cdae1b1f2ed29c577539bd300edaaffb6
Hernan Case <her.case@live.com.ar>
2021-03-15 00:07:03 -0300

    move Menu to helpers folder

---
+++ src/helpers/menu.js
@@ -0,0 +1,27 @@
+export const menuItems = [
+  {
+    title: "t-shirts",
+    link: "#",
+    active: true,
+  },
+  {
+    title: "trousers",
+    link: "#",
+    active: true,
+  },
+  {
+    title: "jeans",
+    link: "#",
+    active: true,
+  },
+  {
+    title: "sandals",
+    link: "#",
+    active: true,
+  },
+  {
+    title: "contact",
+    link: "#",
+    active: true,
+  },
+];
